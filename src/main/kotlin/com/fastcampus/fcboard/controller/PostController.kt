0dfbc2cd59db28911676a0b13b58b716993fe1cd
min-yeong <vlrn611@naver.com>
2025-06-14 16:28:56 +0900

    feat : Add read functions for posts

--- src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
+++ src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
@@ -1,6 +1,12 @@
 package com.fastcampus.fcboard.controller
 
-import com.fastcampus.fcboard.controller.dto.*
+import com.fastcampus.fcboard.controller.dto.PostCreateRequest
+import com.fastcampus.fcboard.controller.dto.PostDetailResponse
+import com.fastcampus.fcboard.controller.dto.PostSearchRequest
+import com.fastcampus.fcboard.controller.dto.PostSummaryResponse
+import com.fastcampus.fcboard.controller.dto.PostUpdateRequest
+import com.fastcampus.fcboard.controller.dto.toDto
+import com.fastcampus.fcboard.controller.dto.toResponse
 import com.fastcampus.fcboard.service.PostService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
