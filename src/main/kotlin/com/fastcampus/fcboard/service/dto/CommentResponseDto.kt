9d4a879b16ed98453b84d03cbfe26d5ae7e66ac9
min-yeong <vlrn611@naver.com>
2025-06-15 21:50:22 +0900

    feat : Add read functions for comments

--- src/main/kotlin/com/fastcampus/fcboard/service/dto/CommentResponseDto.kt
+++ src/main/kotlin/com/fastcampus/fcboard/service/dto/CommentResponseDto.kt
@@ -2,17 +2,16 @@ package com.fastcampus.fcboard.service.dto
 
 import com.fastcampus.fcboard.domain.Comment
 
-
 data class CommentResponseDto(
     val id: Long,
     val content: String,
     val createdBy: String,
-    val createdAt: String,
+    val createdAt: String
 )
 
 fun Comment.toResponseDto() = CommentResponseDto(
     id = id,
     content = content,
     createdBy = createdBy,
-    createdAt = createdAt.toString(),
+    createdAt = createdAt.toString()
 )
